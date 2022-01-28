d82d02ee8a26579a1a06faf9cbf0a5301bc4f2b1
mca <jamesalvine340@gmail.com>
2022-01-28 13:30:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="container">
+    
+  {/* <!-- Title --> */}
+  <div className="title text-center"><h2>Todos</h2></div>
+  
+  {/* <!-- Add task --> */}
+  <div className="add-task card" >
+      {/* <!-- div 1 --> */}
+      <div className=" card-header"><span className="fs-5"> Add a task</span></div>
+      {/* <!-- div 2 --> */}
+      <div className="card-body">
+
+          <label for="Item" className="form-label">Add Item</label>
+          <input type="text" className="form-control" id="Item" placeholder="what do you want to do?"/>
+          <div className="form-text">Add what you wish to do.</div>
+          <button type="submit" className="btn btn-primary mt-2">Submit</button>
+      </div>
+  </div>
+  {/* <!--  --> */}
+  <div className="add-task card mt-5" >
+      {/* <!-- div 1 --> */}
+      <div className=" card-header"><span className="fs-5"> Tasks</span></div>
+      {/* <!-- div 2 --> */}
+      <div className="card-body">
+          <div className="row row-cols-3 my-3" >
+              <div className="col-6">
+                  <span>Buy Benz</span>
+                </div>
+                <div className="col-3">
+                  <span>Not Completed</span>
+                </div>
+                <div className="col-3">
+                  <button className="btn btn-primary">Complete</button>
+                  <button className="btn btn-danger">Delete</button>
+                </div>
+            
+          </div>
+          {/* <!-- 1 --> */}
+          <div className="row row-cols-3 my-3" >
+              <div className="col-6">
+                  <span>Buy Benz</span>
+                </div>
+                <div className="col-3">
+                  <span>Not Completed</span>
+                </div>
+                <div className="col-3">
+                  <button className="btn btn-primary">Complete</button>
+                  <button className="btn btn-danger">Delete</button>
+                </div>
+            
+          </div>
+          {/* <!--  --> */}
+          <div className="row row-cols-3 my-3" >
+              <div className="col-6">
+                  <span>Buy Benz</span>
+                </div>
+                <div className="col-3">
+                  <span>Not Completed</span>
+                </div>
+                <div className="col-3">
+                  <button className="btn btn-primary">Complete</button>
+                  <button className="btn btn-danger">Delete</button>
+                </div>
+            
+          </div>
+          {/* <!--  --> */}
+          <div className="row row-cols-3 my-3" >
+              <div className="col-6">
+                  <span>make more money</span>
+                </div>
+                <div className="col-3">
+                  <span>Not Completed</span>
+                </div>
+                <div className="col-3">
+                  <button className="btn btn-primary">Complete</button>
+                  <button className="btn btn-danger">Delete</button>
+                </div>
+                
+            
+          </div>
+          {/* <!--  --> */}
+          <div className="row row-cols-3 my-3" >
+              <div className="col-6">
+                  <span>Buy Dollars</span>
+                </div>
+                <div className="col-3">
+                  <span>Not Completed</span>
+                </div>
+                <div className="col-3">
+                  <button className="btn btn-primary">Complete</button>
+                  <button className="btn btn-danger">Delete</button>
+                </div>
+            
+          </div>
+      </div>
+  </div></div>;
 }
 
 export default App;
---
+++ src/file.js
@@ -0,0 +1,54 @@
+
+
+const App=()=> {
+    let name="another";
+    const phonenumber= 89894232; 
+    let isMale=true
+    let group=["Simon","Garry","Sarah"]
+    // this is a group of 3 pple
+    // console.log("this is our group",group)
+    let i =0;
+  
+  // function showAge(){
+  //   return 20
+  // }
+    // function ourFunction(){
+    //  console.log("my name is",name)
+    // }
+  
+    // let ourFunction = ()=>console.log("I am a function")
+  
+    const showAge = ()=>{
+      // console.log("Hello)
+    }
+    // ourFunction()
+    // console.log(showAge());
+    // showAge(z)
+  
+    // ourFunction("jina")
+    // console.log(group[1])
+    // while (i<3)
+    //  {
+    //    console.log(group[i]);
+    //    i++;
+    // }
+  
+    // For loop:
+    // for(let i=0; i<3; i++){
+      // console.log(group[i]);
+    // }
+  
+  //   group.forEach(function(item){
+  // console.log(item)
+  //   })
+    
+    // console.log("this is ",name,"phonenumber",phonenumber,"isMale",true)
+    return (<></>)
+    //   <div className="App">
+    //     <h2>MY FIRST REACT APP</h2>
+        
+    //   </div>
+    // );
+  }
+  
+  export default App;
